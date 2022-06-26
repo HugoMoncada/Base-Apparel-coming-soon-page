78360d4a8cacd53cedc40a513d5bf262e0c9b50b
Hugo Moncada <hugo_moncada@outlook.com>
2022-06-26 15:19:59 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const response = document.querySelector(".response");
+const validRegex = /\S+@\S+\.\S+/;
+const errorIcon = document.querySelector(".error-icon");
+const input = document.querySelector("input");
+
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+
+    const valid = validRegex.test(input.value);
+
+    if(valid == false){
+        response.innerHTML= "please provide a valid email";
+        response.style.color = "red"
+        errorIcon.style.display= "block";
+        input.style.border = "1px solid red";
+        return;
+    }
+    errorIcon.style.display= "none";
+    response.innerHTML= "Thank you we'll be in touch soon!";
+    response.style.color = "green"
+    input.style.border = "1px solid var(--Desaturate-Red)";
+
+});
